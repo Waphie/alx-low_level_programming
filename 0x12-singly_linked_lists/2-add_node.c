e9b05c4b326f1acbd63a226a7e04706bf91d9c9a
Waphie <siwaphiwehlazo@gmail.com>
2023-11-17 15:21:15 -0800

    Updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,63 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
 
-/**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
- *
- * Return: the address of the new element, or NULL if it fails
- */
-list_t *add_node(list_t **head, const char *str)
-{
- list_t *new;
- unsigned int len = 0;
+typedef struct Node {
+    char *str;
+    struct Node *next;
+} list_t;
 
- while (str[len])
- len++;
+list_t *add_node(list_t **head, const char *str) {
+    // Check if the input string is not NULL
+    if (str == NULL) {
+        return NULL;
+    }
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+    // Allocate memory for the new node
+    list_t *new_node = malloc(sizeof(list_t));
+    if (new_node == NULL) {
+        return NULL; // Allocation failed
+    }
 
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
+    // Duplicate the input string using strdup
+    new_node->str = strdup(str);
+    if (new_node->str == NULL) {
+        free(new_node);
+        return NULL; // strdup failed
+    }
 
- return (*head);
+    // Set the next pointer of the new node to the current head
+    new_node->next = *head;
+
+    // Update the head to point to the new node
+    *head = new_node;
+
+    // Return the address of the new element
+    return new_node;
+}
+
+int main() {
+    list_t *head = NULL;
+
+    // Example usage
+    add_node(&head, "Class");
+    add_node(&head, "Hey");
+
+    // Print the elements in the list
+    list_t *current = head;
+    while (current != NULL) {
+        printf("%s ", current->str);
+        current = current->next;
+    }
+
+    // Free the memory allocated for the list
+    current = head;
+    while (current != NULL) {
+        list_t *temp = current;
+        current = current->next;
+        free(temp->str);
+        free(temp);
+    }
+
+    return 0;
 }
