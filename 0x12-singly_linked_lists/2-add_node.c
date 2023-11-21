2c491f4ca9b916d62cf8eb483a9e202db0e3e98c
Waphie <siwaphiwehlazo@gmail.com>
2023-11-21 07:52:44 -0800

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,63 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "lists.h"
 
-typedef struct Node {
-    char *str;
-    struct Node *next;
-} list_t;
+/**
+ * add_node - Adds a newnode at the beginning of a list
+ * @head: The original linked list
+ * @str: The string to add to the node
+ *
+ * Return: The address of the new list or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
 
-list_t *add_node(list_t **head, const char *str) {
-    // Check if the input string is not NULL
-    if (str == NULL) {
-        return NULL;
-    }
+	list_t *temp;
+	int length = 0;
 
-    // Allocate memory for the new node
-    list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL) {
-        return NULL; // Allocation failed
-    }
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+	      return (NULL);
+	
+	      while (str[length])
+	                length++;
 
-    // Duplicate the input string using strdup
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node);
-        return NULL; // strdup failed
-    }
-
-    // Set the next pointer of the new node to the current head
-    new_node->next = *head;
-
-    // Update the head to point to the new node
-    *head = new_node;
-
-    // Return the address of the new element
-    return new_node;
-}
-
-int main() {
-    list_t *head = NULL;
-
-    // Example usage
-    add_node(&head, "Class");
-    add_node(&head, "Hey");
-
-    // Print the elements in the list
-    list_t *current = head;
-    while (current != NULL) {
-        printf("%s ", current->str);
-        current = current->next;
-    }
-
-    // Free the memory allocated for the list
-    current = head;
-    while (current != NULL) {
-        list_t *temp = current;
-        current = current->next;
-        free(temp->str);
-        free(temp);
-    }
-
-    return 0;
-}
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp
+	return (temp);
+}	
